424524abb9d4cf9d741b8349cf64539ca5cb7ed9
chenguitong <chenguitong97@163.com>
2022-09-22 17:57:54 +0800

    商户发布商品测评;商户对测评申请进行审核; 用户申请测评

---
+++ src/main/java/modules/mapper/EvaluationApplyMapper.java
@@ -0,0 +1,16 @@
+package modules.mapper;
+
+import modules.entity.EvaluationApply;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 测评商品申请表 Mapper 接口
+ * </p>
+ *
+ * @author chenguitong
+ * @since 2022-09-22
+ */
+public interface EvaluationApplyMapper extends BaseMapper<EvaluationApply> {
+
+}
